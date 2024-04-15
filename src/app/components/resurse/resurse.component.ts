064aafbba4c18d124cdb832135506ca33c2bd303
rubenNoris <94965538+rubenNoris@users.noreply.github.com>
2024-04-15 19:40:09 +0300

    DA

--- src/app/components/resurse/resurse.component.ts
+++ src/app/components/resurse/resurse.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-resurse',
+  // standalone: true,
+  // imports: [CommonModule, RouterModule],
   templateUrl: './resurse.component.html',
-  styleUrl: './resurse.component.css'
+  styleUrl: './resurse.component.css',
 })
-export class ResurseComponent {
-
-}
+export class ResurseComponent {}
