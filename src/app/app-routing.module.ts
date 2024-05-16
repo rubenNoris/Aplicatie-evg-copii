a067c8645547043c0ace321d1aaf37139e9af574
rubenNoris <94965538+rubenNoris@users.noreply.github.com>
2024-05-16 22:10:23 +0300

    editari din alea blanao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { CantececComponent } from './components/cantecec/cantecec.component';
 import { CompetitiiComponent } from './components/competitii/competitii.component';
 import { LectiiComponent } from './components/lectii/lectii.component';
@@ -7,19 +6,18 @@ import { JocuriComponent } from './components/jocuri/jocuri.component';
 import { ResurseComponent } from './components/resurse/resurse.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
     path: 'resurse',
     component: ResurseComponent,
-    children: [
-      { path: 'jocuri', component: JocuriComponent },
-      { path: 'competitii', component: CompetitiiComponent },
-      { path: 'cantece', component: CantececComponent },
-      { path: 'lectii', component: LectiiComponent },
-    ],
   },
+  { path: 'jocuri', component: JocuriComponent },
+  { path: 'competitii', component: CompetitiiComponent },
+  { path: 'cantece', component: CantececComponent },
+  { path: 'lectii', component: LectiiComponent },
 ];
 
 @NgModule({
