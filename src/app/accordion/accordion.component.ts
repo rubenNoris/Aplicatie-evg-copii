70caa8a3dbe7dfad87c7f161b6c9156af443cc45
rubenNoris <94965538+rubenNoris@users.noreply.github.com>
2024-06-17 15:40:07 +0300

    front ts-uri editare cantari

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,25 +1,48 @@
-import { Component, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.css'],
 })
-export class AccordionComponent implements AfterViewInit {
-  @ViewChild('accordionButton') accordionButton!: ElementRef<HTMLButtonElement>;
+export class AccordionComponent {
+  filter: string = '';
+  accordions: any[] = [
+    {
+      id: 1,
+      title: '1. laudati ',
+      content: ` /: /:Lăudaţi pe Isus! Lăudaţi pe Isus! Cu braţ puternic El domneşte.
+      Lăudaţi pe Isus! Lăudaţi pe Isus! Suntem poporul ce-l iubeşte:/ Suntem
+      poporul Celui ce-a înviat El este Rege!Isus ne-a salvat Şi din nou va spun
+      :/`,
+      tags: ['tag1', 'tag2'],
+      isOpen: false,
+    },
+    {
+      id: 2,
+      title: 'Acordeon 2',
+      content: 'Conținut pentru Acordeon 2',
+      tags: ['tag2', 'tag3'],
+      isOpen: false,
+    },
+    // Alte acordeoane...
+  ];
 
-  constructor() {}
+  toggleAccordion(accordion: any): void {
+    if (!this.isFilteredOut(accordion)) {
+      accordion.isOpen = !accordion.isOpen;
+      this.accordions.forEach((a) => {
+        if (a !== accordion) a.isOpen = false;
+      });
+    }
+  }
 
-  ngAfterViewInit(): void {
-    this.accordionButton.nativeElement.addEventListener('click', () => {
-      const panel = this.accordionButton.nativeElement
-        .nextElementSibling as HTMLElement;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = '';
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + 'px';
-      }
-      this.accordionButton.nativeElement.classList.toggle('active');
-    });
+  setFilter(filter: string): void {
+    this.filter = filter.toLowerCase();
+  }
+
+  isFilteredOut(accordion: any): boolean {
+    if (!this.filter) return false;
+    return !accordion.tags.includes(this.filter);
   }
 }
